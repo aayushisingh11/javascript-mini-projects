86b31ae7a18a7483003f10bb0a102f6d98d1e5f2
aayushisingh11 <aayushisingh881@gmail.com>
2021-08-02 10:10:48 +0530

    cat gen compl

--- All in one 5 games/js/script.js
+++ All in one 5 games/js/script.js
@@ -13,3 +13,10 @@ function ageInDays(){
 function reset(){
     document.getElementById('ageInDays').remove();
 }
+//Game 2 - Generate Cat
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
